77dd489fa66e98a14e6000e13b594a6fd97397dc
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-20 17:12:02 +0600

    add retrofit client

---
+++ app/src/main/java/com/timplifier/dotaapp/data/remote/RetrofitClient.kt
@@ -0,0 +1,30 @@
+package com.timplifier.dotaapp.data.remote
+
+import com.timplifier.dotaapp.common.constants.Constants.BASE_URL
+import com.timplifier.dotaapp.data.remote.apis.HeroesApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class RetrofitClient {
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(provideLoggingInterceptor())
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    private fun provideLoggingInterceptor() =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
+        .build()
+
+    fun provideHeroesApiService(): HeroesApiService = retrofit.create(HeroesApiService::class.java)
+
+}
