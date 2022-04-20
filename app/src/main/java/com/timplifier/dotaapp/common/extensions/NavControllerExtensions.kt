1782993296dbea36ca31d6c95ea788b91ccc4608
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-20 17:21:02 +0600

    navcontroller extensions in order to prevent app from crashing

---
+++ app/src/main/java/com/timplifier/dotaapp/common/extensions/NavControllerExtensions.kt
@@ -0,0 +1,12 @@
+package com.timplifier.dotaapp.common.extensions
+
+import androidx.annotation.IdRes
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+
+
+fun NavController.navigateSafely(@IdRes actionId: Int) =
+    currentDestination?.getAction(actionId)?.let { navigate(actionId) }
+
+fun NavController.navigateSafelyUsingDirections(directions: NavDirections) =
+    currentDestination?.getAction(directions.actionId)?.let { navigate(directions) }
