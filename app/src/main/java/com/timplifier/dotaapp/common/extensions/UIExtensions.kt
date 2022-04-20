3402b76630637c99a9d55c77de1f818bbb76c127
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-20 17:05:41 +0600

    add an ImageView's extension to load images with glide

--- app/src/main/java/com/timplifier/dotaapp/common/extensions/UIExtensions.kt
+++ app/src/main/java/com/timplifier/dotaapp/common/extensions/UIExtensions.kt
@@ -1,2 +1,6 @@
 package com.timplifier.dotaapp.common.extensions
 
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+fun ImageView.loadImageWithGlide(url: String) = Glide.with(this).load(url).into(this)
